3a1c687b5c725f4869aaa039f9ad8a026919757d
Joan Ciprià <joancipria@protonmail.com>
2022-10-17 13:38:16 +0200

    Some fixes and comments

--- train.py
+++ train.py
@@ -3,52 +3,64 @@ from datasets import load_dataset
 
 dataset = load_dataset("EmoEvent", use_auth_token=True)
 
-# Tokenize dataset
-from transformers import AutoTokenizer
+# List of spanish pre-trained models to fine-tune
+pre_trained_models = ["PlanTL-GOB-ES/roberta-base-bne", "PlanTL-GOB-ES/roberta-large-bne",
+                      "PlanTL-GOB-ES/roberta-base-biomedical-es", "PlanTL-GOB-ES/gpt2-base-bne", "PlanTL-GOB-ES/gpt2-large-bne"]
 
-tokenizer = AutoTokenizer.from_pretrained("PlanTL-GOB-ES/roberta-base-bne")
+# For each pre-trained model, fine-tune it
+for PRE_TRAINED_MODEL in pre_trained_models:
 
-def tokenize_function(examples):
-    return tokenizer(examples["tweet"], padding="max_length", truncation=True)
- 
-tokenized_datasets = dataset.map(tokenize_function, batched=True)
+    # Tokenize dataset
+    from transformers import AutoTokenizer
 
-# Load pretrained model
-from transformers import AutoModelForSequenceClassification
-checkpoint = "PlanTL-GOB-ES/roberta-base-bne"
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=7)
+    tokenizer = AutoTokenizer.from_pretrained(PRE_TRAINED_MODEL)
 
-# Define evaluation metrics
-import numpy as np
-from datasets import load_metric
- 
-metric = load_metric("accuracy")
- 
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
+    def tokenize_function(examples):
+        return tokenizer(examples["tweet"], padding="max_length", truncation=True, max_length=40)
 
-# Create trainer
-from transformers import TrainingArguments, Trainer
- 
-training_args = TrainingArguments(output_dir="trainer", evaluation_strategy="epoch", num_train_epochs=10)
- 
- 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["test"],
-    compute_metrics=compute_metrics,
-)
+    tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-# Train 
-trainer.train()
+    # Load pre-trained model
+    from transformers import AutoModelForSequenceClassification
+    checkpoint = PRE_TRAINED_MODEL
+    model = AutoModelForSequenceClassification.from_pretrained(
+        checkpoint, num_labels=7)
+    model.resize_token_embeddings(len(tokenizer))
 
-# Save model
-model.save_pretrained("models/FineTunedEmoEvent")
-# alternatively save the trainer
-# trainer.save_model("models/FineTunedEmoEvent")
-tokenizer.save_pretrained("models/FineTunedEmoEvent")
+    # Define evaluation metrics
+    import numpy as np
+    from datasets import load_metric
+
+    metric = load_metric("accuracy")
+
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    # Create trainer
+    from transformers import TrainingArguments, Trainer
+
+    training_args = TrainingArguments(
+        output_dir="trainer", evaluation_strategy="epoch", num_train_epochs=10)
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["test"],
+        # compute_metrics=compute_metrics,
+    )
+
+    # Train
+    trainer.train()
+
+    # Save model
+    fine_tuned_model_output = "models/"+PRE_TRAINED_MODEL+"-FineTunedEmoEvent"
+    model.save_pretrained(fine_tuned_model_output)
+    # alternatively save the trainer
+    # trainer.save_model("models/FineTunedEmoEvent")
+    tokenizer.save_pretrained(fine_tuned_model_output)
