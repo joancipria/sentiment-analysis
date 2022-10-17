ed1948b05c786fb540924850b5560575a010bbee
Joan Ciprià <joancipria@protonmail.com>
2022-10-17 13:38:30 +0200

    Add comments

--- predict.py
+++ predict.py
@@ -1,15 +1,20 @@
-# Load the model
-from transformers import AutoTokenizer
+from transformers import pipeline
 from transformers import AutoModelForSequenceClassification
- 
-load_model = AutoModelForSequenceClassification.from_pretrained("models/FineTunedEmoEvent")
- 
-load_tokenizer = AutoTokenizer.from_pretrained("models/FineTunedEmoEvent")
+from transformers import AutoTokenizer
+
+# Select model
+FINE_TUNED_MODEL = "models/PlanTL-GOB-ES/roberta-large-bne-FineTunedEmoEvent"
+
+# Load the model
+load_model = AutoModelForSequenceClassification.from_pretrained(
+    FINE_TUNED_MODEL)
+
+load_tokenizer = AutoTokenizer.from_pretrained(FINE_TUNED_MODEL)
 
 # Setup pipeline
-from transformers import pipeline
-my_pipeline  = pipeline("sentiment-analysis", model=load_model, tokenizer=load_tokenizer)
+my_pipeline = pipeline("sentiment-analysis",
+                       model=load_model, tokenizer=load_tokenizer)
 
 # Predict sentiment for the following text
-text = ["estoy muy disgustado"] 
+text = ["estoy muy disgustado"]
 print(my_pipeline(text))
